6eaf8675e8193b2c01c0b0d9e2c7418b49f429b0
Duane0307 <duduane02@hotmail.com>
2025-09-02 00:35:06 +0000

    feat: add projeto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Televisao tv = new Televisao (55f);
+
+Console.WriteLine($"a tv tem o tamanho {tv.Tamanho}");
