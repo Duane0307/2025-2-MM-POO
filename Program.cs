aa79447349dbc0dda21b3010050912a6375560b9
Duane0307 <duduane02@hotmail.com>
2025-09-02 00:57:17 +0000

    feat: add projeto

--- Program.cs
+++ Program.cs
@@ -4,3 +4,11 @@ Console.WriteLine("Hello, World!");
 Televisao tv = new Televisao (55f);
 
 Console.WriteLine($"a tv tem o tamanho {tv.Tamanho}");
+
+
+tv.AumentarVolume();
+
+tv.AumentarVolume();
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
